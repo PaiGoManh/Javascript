3f82d8a890a116a55a5d9cb2d81106b39520bbe4
PaiGoManh <rahulrahulsajeevan007@gmail.com>
2024-05-28 08:16:20 +0530

    first commit

---
+++ Rahul-JavaScript-Question-21.may-24/Q1-perimeter-circle-square-rectangle/q1-function.js
@@ -0,0 +1,21 @@
+{
+    function Per1(r){
+        result=2*3.14*r;
+        document.getElementById("perc").innerHTML=`Perimeter :`+result;
+    }
+}
+
+{
+    function Per2(l,b){
+        result=(l+b)*2;
+        document.getElementById("perr").innerHTML=`Perimeter:`+result;
+    }
+    
+}
+{
+    function Per(a){
+        result=4*a;
+        document.getElementById("pers").innerHTML=`Perimeter :`+result;
+    }
+}
+
---
+++ function.js
@@ -0,0 +1,13 @@
+{
+    function add(a,b){
+        result=a+b;
+        document.write(`Addition :`+result)
+    }
+}
+
+{
+    function mul(a,b){
+        result=a*b;
+        document.write("<br><br>Multiply:"+result);
+    }
+}
